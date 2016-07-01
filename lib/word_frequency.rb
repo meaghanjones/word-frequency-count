431076fdde9b1b88af1f45933df5bf999a01ca6a
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-01 09:02:54 -0700

    define function in word_frequency.rb so the web application can determine if there is a match between the word entered and any of the words in the string

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,3 +1,11 @@
 class String
-  define_method(:word_frequency) do
-     
+  def word_frequency (input_word)
+      matched_words_array = []
+    self.split().each do |word|
+      if word.eql?(input_word)
+        matched_words_array.push(input_word)
+      end
+    end
+    matched_words_array
+  end
+end
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -1,5 +1,11 @@
 require('rspec')
-require('word_frequency')
+require('word_frequency.rb')
 
 describe('String#word_frequency') do
-  it
+  it('checks whether a specific word is in a given string') do
+    expect("Meaghan Jones is wonderful".word_frequency("wonderful")).to eq(["wonderful"])
+  end
+  it('checks whether a specific word is in a given string') do
+    expect("Meaghan Jones is really really wonderful".word_frequency("really")).to eq(["really", "really"])
+  end
+end
