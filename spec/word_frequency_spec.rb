fc3e8bd769ec6e8310938086620a7e81f062267d
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-01 12:58:49 -0700

    change the word frequency method so the results page shows the inputted string with the original punctuation

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -2,10 +2,10 @@ require('rspec')
 require('word_frequency.rb')
 
 describe('String#word_frequency') do
-  it('checks whether a specific word is in a given string') do
+  it('checks whether a given word is in a given string') do
     expect("Meaghan Jones is wonderful".word_frequency("wonderful")).to eq(1)
   end
-  it('checks whether a specific word is in a given string') do
+  it('checks how many times a given word is in a given string') do
     expect("Meaghan Jones is really really wonderful".word_frequency("really")).to eq(2)
   end
   it('matches a word even if the input word is a different case than the inputted phrase') do
