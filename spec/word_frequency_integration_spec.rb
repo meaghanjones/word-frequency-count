c91824d5e73e6e24c9c40de0cda0b79d1dfd7378
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-01 10:42:52 -0700

    add integration test for the word frequency path

--- spec/word_frequency_integration_spec.rb
+++ spec/word_frequency_integration_spec.rb
@@ -8,6 +8,7 @@ describe('the word frequency path', {:type => :feature}) do
     visit('/')
     fill_in('input-phrase', :with => 'How much wood could a wood chuck chuck if a wood chuck could chuck wood')
     fill_in('input-word', :with => 'wood')
-    expect(page).to have_content("Your word occurs in the phrase 4 times.")
+    click_button('Submit')
+    expect(page).to have_content(4)
   end
 end
